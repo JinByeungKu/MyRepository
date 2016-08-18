b0877cbcd274f86c515d978770806dcb7557ce1c
Bziaco <bjh9222@naver.com>
2016-08-18 16:37:52 +0900

    exam02 소스 추가

---
+++ JavaProgramming/src/ch02/exam02/Casting.java
@@ -0,0 +1,26 @@
+package ch02.exam02;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		long v1 = 300;
+		int v2 = (int)v1;
+		System.out.println(v2);
+		
+		byte v3 = (byte) v1;
+		System.out.println(v3);
+		
+		
+		char v5 = 'วั';
+		int v6 = 'A';
+		int v7 = 'วั';
+		System.out.println(v5);
+		System.out.println(v6);
+		System.out.println(v7);
+		
+		int v8 = 65;
+		char v9 = (char) v8;
+		System.out.println(v9);
+	}
+
+}
---
+++ JavaProgramming/src/ch02/exam02/OperationPromotion.java
@@ -0,0 +1,18 @@
+package ch02.exam02;
+
+public class OperationPromotion {
+	public static void main(String args[]){
+		int v1 = 10 + 20;
+		System.out.println(v1);
+		
+		byte v2 = 10;
+		byte v3 = 20;
+		//int v4 = v2 + v3;
+		int v5 = v2 + v3;
+		System.out.println(v5);
+		
+		char v6 ='A';
+		char v7 = (char) (v6 + 1);
+		System.out.println(v7);
+	}
+}
