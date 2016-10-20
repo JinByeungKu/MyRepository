8cfad5ca78bcc0e42826c77633c6ba2f839f7d74
JinByeungKu <splendid1014@naver.com>
2016-10-20 18:50:16 +0900

    10.20

--- SqlProgramming/src/finalsource/dao/BoardDao.java
+++ SqlProgramming/src/finalsource/dao/BoardDao.java
@@ -10,87 +10,97 @@ import java.util.List;
 
 import finalsource.dto.Board;
 
+
 public class BoardDao {
-	private Connection conn;
-	public void setConn(Connection conn) {
-		this.conn = conn;
-	}
-	
-	public  int insert(Board board) throws SQLException {
-		String sql = "insert into member(bno, btitle, bcontent, bwriter, bhitcount, bdate) values(?,?,?,?,?,?)";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, board.getBno());
-		pstmt.setString(2,  board.getBtitle());
-		pstmt.setString(3,  board.getBcontent());
-		pstmt.setString(4,  board.getBwriter());
-		pstmt.setInt(5,  board.getBhitcount());
-		pstmt.setDate(6, new Date(board.getBdate().getTime()));
-		int rowNo = pstmt.executeUpdate();
-		pstmt.close();
-		return rowNo;
-	}
-	
-	public Board selectByBno(int bno) throws SQLException {
-		String sql = "select bno, btitle, bcontent, bwriter, bhitcount, bdate from board where bno=?";
-		Board board = null;
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, bno);
-		ResultSet rs = pstmt.executeQuery();
-		if(rs.next()) {
-			board = new Board();
-			board.setBno(rs.getInt("bno"));
-			board.setBtitle(rs.getString("btitle"));
-			board.setBcontent(rs.getString("bcontent"));
-			board.setBwriter(rs.getString("bwriter"));
-			board.setBhitcount(rs.getInt("bhitcount"));
-			board.setBdate(rs.getDate("bdate"));
-		}
-		rs.close();
-		pstmt.close();
-		return board;
-	}
-	
-	public List<Board> selectByBtitle(String btitle) throws SQLException {
-		String sql = "select bno, btitle, bcontent, bwriter, bhitcount, bdate from board where btitle like ?";
-		List<Board> list = new ArrayList<>();
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, "%" + btitle + "%");
-		ResultSet rs = pstmt.executeQuery();
-		while(rs.next()) {
-			Board board = new Board();
-			board.setBno(rs.getInt("bno"));
-			board.setBtitle(rs.getString("btitle"));
-			board.setBcontent(rs.getString("bcontent"));
-			board.setBwriter(rs.getString("bwriter"));
-			board.setBhitcount(rs.getInt("bhitcount"));
-			board.setBdate(rs.getDate("bdate"));
-			list.add(board);
-		}
-		rs.close();
-		pstmt.close();
-		return list;
-	}
-	
-	public int update(Board board) throws SQLException {
-		String sql = "update board set btitle=?, bcontent=?, bwriter=?, bhitcount=?, bdate=? where bno=?";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1,  board.getBtitle());
-		pstmt.setString(2,  board.getBcontent());
-		pstmt.setString(3,  board.getBwriter());
-		pstmt.setInt(4,  board.getBhitcount());
-		pstmt.setDate(5, new Date(board.getBdate().getTime()));
-		pstmt.setInt(6, board.getBno());
-		int rowNo = pstmt.executeUpdate();
-		pstmt.close();
-		return rowNo;
-	}
-	
-	public int deleteByBno(int bno) throws SQLException {
-		String sql = "delete board where bno=?";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1,  bno);
-		int rowNo = pstmt.executeUpdate();
-		pstmt.close();
-		return rowNo;
-	}
+   private Connection conn= null;
+   
+   
+   public void setConn(Connection conn) {
+      this.conn = conn;
+   }
+
+   public int Insert(Board board) throws SQLException {
+      String sql = "insert into board(bno,btitle,bcontent,bwriter,bhitcount,bdate) values(seq_board_bno.nextval,?,?,?,0,sysdate)";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, board.getBtitle());
+      pstmt.setString(2, board.getBcontent());
+      pstmt.setString(3, board.getBwriter());
+      int rowNo = pstmt.executeUpdate();
+      pstmt.close();
+      
+      return rowNo;
+   }
+
+   // id는 고유하기떄문에 한개의 행이 가져오거나 아니면 아무것도 안가져온다 그래서 Member객체를 씀
+   public Board selectByMid(int bno) throws SQLException {
+      String sql = "select bno,btitle,bcontent,bwriter,bhitcount,bdate from board where bno=?";
+      Board board = null;
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setInt(1, bno);
+      ResultSet rs = pstmt.executeQuery();
+      
+      //bno pk이기 떄문이다 
+      if(rs.next()){
+         board = new Board();
+         board.setBno(rs.getInt("bno"));
+         board.setBtitle(rs.getString("btitle"));
+         board.setBcontent(rs.getString("bcontent"));
+         board.setBwriter(rs.getString("bwriter"));
+         board.setBhitcount(rs.getInt("bhitcount"));
+         board.setBdate(rs.getDate("bdate"));
+      }
+      rs.close();
+      pstmt.close();
+      return board;
+   }
+   
+   
+   public List<Board> selectByBtitle(String btitle) throws SQLException{
+      List<Board> list = new ArrayList<Board>();
+      String sql = "select bno,btitle,bcontent,bwriter,bhitcount,bdate from board where btitle like ?";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1,"%" + btitle + "%");
+      
+      ResultSet rs = pstmt.executeQuery();
+      
+      while(rs.next()){
+         Board board = new Board();
+         board.setBno(rs.getInt("bno"));
+         board.setBtitle(rs.getString("btitle"));
+         board.setBcontent(rs.getString("bcontent"));
+         board.setBwriter(rs.getString("bwriter"));
+         board.setBhitcount(rs.getInt("bhitcount"));
+         board.setBdate(rs.getDate("bdate"));
+         list.add(board);
+      }
+      rs.close();
+      pstmt.close();
+      return list;
+      
+   }
+   
+   public int Update(Board board) throws SQLException{
+      String sql = "update board set btitle =?, bcontent=?, bwriter=?, bhitcount=?, bdate=? where bno=?";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      
+      pstmt.setString(1, board.getBtitle());
+      pstmt.setString(2, board.getBcontent());
+      pstmt.setString(3, board.getBwriter());
+      pstmt.setInt(4, board.getBhitcount());
+      pstmt.setDate(5, new Date(board.getBdate().getTime()));
+      pstmt.setInt(6,board.getBno());
+
+      int rowNo = pstmt.executeUpdate();
+      pstmt.close();
+      return rowNo;
+   }
+   
+   public int Delete(int bno) throws SQLException{
+      String sql = "delete from board where bno=?";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setInt(1, bno);
+      int rowNo = pstmt.executeUpdate();
+      pstmt.close();
+      return 0;
+   }
 }
--- SqlProgramming/src/finalsource/service/TestBoardDao.java
+++ SqlProgramming/src/finalsource/service/TestBoardDao.java
@@ -4,71 +4,159 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import finalsource.dao.BoardDao;
+
 import finalsource.dto.Board;
+
 
 public class TestBoardDao {
 
-	public static void main(String[] args) {
-		//testInsert();
-		//testSelectByBno();
-		//testSelectByMname();
-		//testUpdate();
-		//testDeleteByMid();
-	}
-	
-	public static void testInsert() {
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","tester1","kosa12345");
-		
-			BoardDao dao = new BoardDao();
-			dao.setConn(conn);
-		
-			Board board = new Board();
-			board.setBno(10);
-			board.setBtitle("제목10");
-			board.setBcontent("내용10");
-			board.setBwriter("user10");
-			board.setBhitcount(10);
-			board.setBdate(new Date());
-			
-			int rowNo = dao.insert(board);
-			System.out.println(rowNo + "행이 저장됨");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try { conn.close(); } catch (SQLException e) { }
-		}
-	}
-	
-	public static void testSelectByBno() {
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","tester1","kosa12345");
-			
-			BoardDao dao = new BoardDao();
-			dao.setConn(conn);
-			
-			Board board = dao.selectByBno(10);
-			if(board != null) {
-				System.out.print(board.getBno() + ":");
-				System.out.print(board.getBtitle() + ":");
-				System.out.print(board.getBcontent() + ":");
-				System.out.print(board.getBwriter() + ":");
-				System.out.print(board.getBhitcount() + ":");
-				System.out.print(board.getBdate() + ":");
-				System.out.println();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try { conn.close(); } catch (SQLException e) {}
-		}
-	}
-	
-	
+   public static void main(String[] args) {
+	   testInsert();
+       //testselectByMid();
+       //testselectByMname();
+       //testupdate();
+       //testdeleteByMid();
+   }
+   
+   public static void testInsert(){
+      Connection conn = null;
+
+      try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+
+         BoardDao dao = new BoardDao();
+         dao.setConn(conn);
+
+         Board board = new Board();
+         board.setBno(1);
+         board.setBtitle("테스트 제목1");
+         board.setBcontent("테스트 내용1");
+         board.setBwriter("user10");
+         
+         int rowNo = dao.Insert(board);
+         System.out.println(rowNo + "행이 저장됨");
+         
+         
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            conn.close();} catch (SQLException e) {}
+      }
+   }
+   public static void testselectByMid(){
+      Connection conn = null;
+
+      try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+
+         BoardDao dao = new BoardDao();
+         dao.setConn(conn);
+         
+         Board board = dao.selectByMid(1);
+         if(board != null){
+            System.out.println(board.getBno() + ":");
+            System.out.println(board.getBtitle() + ":");
+            System.out.println(board.getBcontent() + ":");
+            System.out.println(board.getBwriter() + ":");
+            System.out.println(board.getBhitcount() + ":");
+            System.out.println(board.getBdate() + ":");
+            System.out.println();
+         }
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            conn.close();} catch (SQLException e) {}
+      }
+   }
+   public static void testselectByMname(){
+      Connection conn = null;
+
+      try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+
+         BoardDao dao = new BoardDao();
+         dao.setConn(conn);
+         
+         List<Board> list = dao.selectByBtitle("10");
+         for(Board board : list){
+            System.out.println(board.getBno() + ":");
+            System.out.println(board.getBtitle() + ":");
+            System.out.println(board.getBcontent() + ":");
+            System.out.println(board.getBwriter() + ":");
+            System.out.println(board.getBhitcount() + ":");
+            System.out.println(board.getBdate() + ":");
+            System.out.println();
+         }
+         
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            conn.close();} catch (SQLException e) {}
+      }
+   }
+   public static void testupdate(){
+      Connection conn = null;
+
+      try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+
+         BoardDao dao = new BoardDao();
+         dao.setConn(conn);
+
+         Board board = new Board();
+         board.setBno(1);
+         board.setBtitle("테스트 제목2");
+         board.setBcontent("테스트 내용2");
+         board.setBwriter("user10");
+         board.setBhitcount(5);
+         board.setBdate(new Date());
+         
+         int rowNo = dao.Update(board);
+         System.out.println(rowNo + "행이 수정됨");
+         
+         
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            conn.close();} catch (SQLException e) {}
+      }
+   }
+   public static void testdeleteByMid(){
+      Connection conn = null;
+
+      try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+         
+         BoardDao dao = new BoardDao();
+         dao.setConn(conn);
+         
+         int rowNo = dao.Delete(1);
+         System.out.println(rowNo + "행이 삭제됨");
+         
+         
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            conn.close();} catch (SQLException e) {}
+      }
+   }
+
 }
