d33ad26a01c23c3a34b9005768c4a47c0436edbb
JinByeungKu <splendid1014@naver.com>
2016-11-08 18:48:24 +0900

    11.08

--- AndroidProgramming/Exam01_/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ AndroidProgramming/Exam01_/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("mylog", "onCreate()");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("mylog", "onStart()");
+    }
+
+    @Override //사용자가 사용할 준비 상태
+    protected void onResume() {
+        super.onResume();
+        Log.i("mylog", "onResume()");
+    }
+
+    @Override //사용자가 사용할 단계
+    protected void onPause() {
+        super.onPause();
+        Log.i("mylog", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("mylog", "onStop()");
+    }
+
+    @Override //화면이 파괴될때
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("mylog", "onDestroy()");
+    }
+
+    public void onClickBtnSecond(View view) {
+        Log.i("mylog", "onClickBtnSecond");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickBtnThird(View view) {
+        Log.i("mylog", "onClickBtnThird");
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
 }
